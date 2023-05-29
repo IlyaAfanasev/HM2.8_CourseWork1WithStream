ffa1b742169dcabeba6c9e2b4a6337f79ba3a315
IlyuAfanasev <gecktor.86@mail.ru>
2023-05-29 22:06:12 +0400

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/hm2_8coursework1withstream/Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hm2_8coursework1withstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
